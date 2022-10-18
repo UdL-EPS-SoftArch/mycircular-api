af1e29325cafea9e934a8c4cea78465364895050
mfa2001 <39986028+mfa2001@users.noreply.github.com>
2022-10-18 16:45:33 +0200

    Added relations of Transaction

--- src/main/java/cat/udl/eps/softarch/demo/domain/Transaction.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Transaction.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
@@ -27,14 +28,16 @@ public class Transaction extends UriEntity<Long> {
     public Long getId() {
         return id;
     }
-    /*
+
     @ManyToOne
+    @NotNull
     private User buyer;
 
     @ManyToOne
+    @NotNull
     private User seller;
 
     @OneToOne
+    @NotNull
     private Announcement announcementAbout;
-     */
 }
