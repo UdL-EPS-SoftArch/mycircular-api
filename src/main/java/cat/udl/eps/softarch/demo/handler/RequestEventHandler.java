899006932653ab491a0c90b1664ed3a7aed4d557
Victor Mateu <victormateulopez@gmail.com>
2022-10-24 16:30:39 +0200

    delete request  test done

--- src/main/java/cat/udl/eps/softarch/demo/handler/RequestEventHandler.java
+++ src/main/java/cat/udl/eps/softarch/demo/handler/RequestEventHandler.java
@@ -5,10 +5,7 @@ import cat.udl.eps.softarch.demo.domain.User;
 import cat.udl.eps.softarch.demo.exception.ForbiddenException;
 import cat.udl.eps.softarch.demo.exception.UnauthorizedException;
 import cat.udl.eps.softarch.demo.repository.RequestRepository;
-import org.springframework.data.rest.core.annotation.Description;
-import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
-import org.springframework.data.rest.core.annotation.HandleBeforeSave;
-import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.data.rest.core.annotation.*;
 import org.springframework.stereotype.Component;
 
 
@@ -55,61 +52,30 @@ public class RequestEventHandler {
     @HandleBeforeCreate
     public void handleRequestPreCreate(Request request) {
         assert request.getId() != null;
-//        if(requestRepository.existsById(request.getId())){
-//            throw new ForbiddenException();
-//        }
+
         String name = request.getName();
         BigDecimal price = request.getPrice();
         String description = request.getDescription();
         User requester = request.getRequester();
         List<Request> requests = requestRepository.findByNameAndPriceAndDescriptionAndRequester(name, price, description, requester);
 
-//        List<Request> pruebaNamePrice = requestRepository.findByNameAndPrice(name, price);
-//        System.out.println(pruebaNamePrice.size());
-//TODO: remember que hay algo de fechas
+        //TODO: remember que hay algo de fechas
 
-
-//        List<Request> pruebaNamePriceDescription = requestRepository.findByNameAndPriceAndDescription(name, price, description);
-//        System.out.println(pruebaNamePriceDescription.size());
-//
-//        System.out.println(requests.size());
 
         if(!requests.isEmpty()) {
             throw new ForbiddenException();
         }
+    }
 
-//        System.out.println("CACAAAAA123");
-//
-//        List<Request> requestsName = requestRepository.findByName(name);
-//        List<Request> requestsPrice = requestRepository.findByPrice(price);
-//        List<Request> requestsDescription = requestRepository.findByDescription(description);
-//        List<Request> requestsRequester = requestRepository.findByRequester(requester);
-//        System.out.println(requestsName.size());
-//        System.out.println(requestsPrice.size());
-//        System.out.println(requestsDescription.size());
-//        System.out.println(requestsRequester.size());
-//        Boolean mirar = requestsName.contains(request) == requestsPrice.contains(request) == requestsDescription.contains(request) && requestsRequester.contains(request);
-//        System.out.println(mirar);
-//        System.out.println(requestsName.contains(request));
+    @HandleBeforeDelete
+    public void handleRequestPreDelete(Request request){
+        assert request.getId() != null;
 
-//        System.out.println(requests);
-//        Boolean comprobar = requests.contains(request);
-//        System.out.println(comprobar);
-//        System.out.println(requests.size());
-//        System.out.println(request);
-//        System.out.println(request.getName());
-//        System.out.println(requests.get(0).getName());
-//        System.out.println(request.getPrice());
-//        System.out.println(requests.get(0).getPrice());
-//        System.out.println(request.getDescription());
-//        System.out.println(requests.get(0).getDescription());
-//        System.out.println(request.getRequester().getUsername());
-//        System.out.println(requests.get(0).getRequester().getUsername());
+        List<Request> requestList = requestRepository.findByRequester(request.getRequester());
 
-//        if(requests.contains(request)) {
-//            System.out.println("CACAAAAA");
-//            throw new UnauthorizedException();
-//        }
+        if(!requestList.contains(request)){
+            throw new ForbiddenException();
+        }
     }
 
 }
