ef1483ecb23666dbd81806fd9d3f6389a2733f4c
VictorMateu <victormateulopez@gmail.com>
2022-11-11 08:57:14 +0100

    Modify own product request (patch)

---
+++ src/test/java/cat/udl/eps/softarch/demo/steps/UpdateProdRequestStepDefs.java
@@ -0,0 +1,74 @@
+package cat.udl.eps.softarch.demo.steps;
+
+import cat.udl.eps.softarch.demo.domain.Request;
+import cat.udl.eps.softarch.demo.domain.User;
+import cat.udl.eps.softarch.demo.exception.NotFoundException;
+import cat.udl.eps.softarch.demo.repository.OfferRepository;
+import cat.udl.eps.softarch.demo.repository.ProdRequestRepository;
+import cat.udl.eps.softarch.demo.repository.RequestRepository;
+import cat.udl.eps.softarch.demo.repository.UserRepository;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+public class UpdateProdRequestStepDefs {
+
+    @Autowired
+    private StepDefs stepDefs;
+    @Autowired
+    private ProdRequestRepository prodRequestRepository;
+    @Autowired
+    private OfferRepository offerRepository;
+    @Autowired
+    private UserRepository userRepository;
+    private Exception e;
+
+    @When("I modify my own created product requests with price {int}")
+    public void iModifyMyOwnCreatedProductRequestsWithPrice(int newPrice) throws Exception {
+        String currentUser = getCurrentUsername();
+        List<Request> myRequests = prodRequestRepository.findByRequester(getUser(currentUser));
+        Request modifiedRequest = myRequests.get(0);
+        Long requestId = modifiedRequest.getId();
+
+        //System.out.println(modifiedRequest);
+
+        stepDefs.result = stepDefs.mockMvc.perform(
+                patch("/prodRequests/{id}", requestId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content((new JSONObject().put("price", new BigDecimal(newPrice))).toString())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate())
+        ).andDo(print());
+    }
+
+    private String getCurrentUsername() {
+        return AuthenticationStepDefs.currentUsername;
+    }
+
+    private User getUser(String username) {
+        Optional<User> users = userRepository.findById(username);
+        if (users.isPresent()) {
+            return users.get();
+        }
+        throw new NotFoundException();
+    }
+
+    @Then("The price of my product request I tried to change is not {int}")
+    public void thePriceOfMyProductRequestITriedToChangeIsNot(int newPrice) {
+        String currentUser = getCurrentUsername();
+        List<Request> myRequests = prodRequestRepository.findByRequester(getUser(currentUser));
+        BigDecimal unexpectedPrice = new BigDecimal(newPrice);
+        Assert.assertNotEquals(unexpectedPrice, myRequests.get(0).getPrice());
+    }
+}
---
+++ src/test/resources/features/UpdateProdRequest.feature
@@ -0,0 +1,20 @@
+Feature: Update Request
+  In order to use the app
+  As a user
+  I want to update requests
+
+  Background:
+    Given There is a registered user with username "user" and password "password" and email "user@sample.app"
+    And There is a registered user with username "nene" and password "password" and email "nene@gmail.com"
+    And I can login with username "user" and password "password"
+    And There is an offer created with name "croqueta2", price 100, description "le hago la competencia a la mama" and offerer named "Paco"
+    And There are 1 offer created
+    And There is a product request created with name "croqueta2", price 100, description "le hago la competencia a la mama" by "user"
+    And There is a product request created with name "croquetas 2.0", price 200, description "mama soy tu mayor fan" by "nene"
+    And There are 2 product request created
+
+  Scenario: Modify own product request (patch)
+    When I modify my own created product requests with price 300
+    Then The response code is 403
+    When I retrieve my own created product requests
+    Then The price of my product request I tried to change is not 300
