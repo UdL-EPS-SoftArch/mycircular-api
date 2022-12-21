7c5f81da65a56b67716f3aaf3c89bd21cca56d78
jnoguero <jrndll2@gmail.com>
2022-12-21 18:11:06 +0100

    Added BBDDInicialitzation for read one Announcement

--- src/main/java/cat/udl/eps/softarch/demo/config/BBDDInicialitzation.java
+++ src/main/java/cat/udl/eps/softarch/demo/config/BBDDInicialitzation.java
@@ -24,5 +24,6 @@ public class BBDDInicialitzation {
         ann.setDescription("blablablablablablabla");
         ann.setPrice(BigDecimal.TEN);
         ann = announcementRepository.save(ann);
+
     }
 }
